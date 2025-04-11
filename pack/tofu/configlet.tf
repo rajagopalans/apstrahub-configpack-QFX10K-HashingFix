9fda0ca407ac85a563fb5ae3226acca14fb3af5c
Rajagopalan Subrahmanian <rajagopalans@gmail.com>
2025-04-11 15:58:14 -0400

    first push

--- pack/tofu/configlet.tf
+++ pack/tofu/configlet.tf
@@ -1,18 +1,14 @@
-resource "apstra_configlet" "example" {
-  name = var.name
+resource "apstra_configlet" "QFX10K_HashingFix" {
+  name = "QFX10K-HashingFix"
   generators = [
     {
-      config_style  = "junos"
-      section       = "top_level_hierarchical"
+      config_style = "junos"
+      section = "top_level_set_delete"
       template_text = <<-EOT
-        ether-options {
-          802.3ad {
-            lacp {
-              force-up;
-            }
-          }
-        }
-      EOT
+set forwarding-options hash-key family inet layer-4
+EOT
+
     },
   ]
+
 }
